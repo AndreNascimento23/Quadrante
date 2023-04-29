0af25d37a06e49b984654cbc54ca68a0117854a1
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-29 19:24:42 -0300

    Programa para achar quadrante cartesiano

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+double x, y;
+
+Console.WriteLine("--- Quadrante Cartesiano ---\n");
+
+
+Console.WriteLine("Entre com o ponto desejado.");
+
+
+Console.Write("Coordenada x: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Coordenada y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+if (x == 0 && y == 0) 
+{
+    Console.WriteLine($"O ponto ({x}, {y}) fica na origem.");
+}
+if (x > 0)
+{
+    if (y < 0)
+    {
+        Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 4.");
+    }
+    else if (y > 0)
+    {
+        Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 1.");
+    }
+}
+if (x < 0)
+{
+    if (y < 0)
+    {
+        Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 3.");
+    }
+    else if (y > 0)
+    {
+        Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 2.");
+    }
+}
+if (x == 0 || y == 0)
+{
+    Console.WriteLine($"O ponto ({x}, {y}) fica no eixo das ordenadas.");
+}
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Quadrante
 Exiba o local onde o ponto se encontra no plano cartesiano
+
+## Exercício `Quadrante`
+
+Leia as coordenadas cartesianas `(x, y)` de um ponto informado pelo usuário.
+
+Exiba o local onde o ponto se encontra no plano cartesiano:
+
+- em um dos quadrantes;
+- sobre um dos eixos;
+- na origem.
+
+Quadrantes:
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Quadrants_01_Pengo.svg/240px-Quadrants_01_Pengo.svg.png)
+
+Exemplos:
+
+```
+--- Quadrante Cartesiano ---
+
+Entre com o ponto desejado.
+Coordenada x: -5
+Coordenada y: 4
+
+O ponto (-5, 4) fica no quadrante 2.
+```
+
+```
+--- Quadrante Cartesiano ---
+
+Entre com o ponto desejado.
+Coordenada x: 0
+Coordenada y: 9
+
+O ponto (0, 9) fica no eixo das ordenadas.
+```
+
+```
+--- Quadrante Cartesiano ---
+
+Entre com o ponto desejado.
+Coordenada x: 0
+Coordenada y: 0
+
+O ponto (0, 0) fica na origem.
